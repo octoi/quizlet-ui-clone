78fc2fa57b9937e34d08e5594d16345d42e421f1
fadhilsaheer <fadhilsaheer@gmail.com>
2021-11-14 19:06:35 +0530

    colors and app bar

---
+++ app/lib/utils/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// colors
+const Color appPrimaryColor = Colors.black;
+const Color appSecondaryColor = Color(0xFF1B2023);
+const Color appTeal = Color(0xFF3BCFCF);
+const Color appGreen = Color(0xFF24B16D);
+const Color appViolet = Color(0xFF4356FB);
